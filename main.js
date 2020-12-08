73e19081d3ac914352c698d7848a40cf8b7cd10c
IamGiel <lfagel84@gmail.com>
2020-12-08 08:59:04 -0500

    using vfor and key

--- main.js
+++ main.js
@@ -3,7 +3,38 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: "/assets/images/socks_green.jpg",
-            inventory: 3
+            inventory: 3,
+            details: [
+                "50% cotton",
+                "30% wool",
+                "20% polyester"
+            ],
+            variants: [
+                {
+                    id:1,
+                    color: 'green'
+                },
+                {
+                    id:2,
+                    color: 'blue'
+                }
+
+            ],
+            sizes: [
+                {
+                    id:1,
+                    name: 'small'
+                },
+                {
+                    id:2,
+                    name: 'medium'
+                },
+                {
+                    id:3,
+                    name: 'large'
+                }
+
+            ]
 
         }
     }
